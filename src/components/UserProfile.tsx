f92e1470859cf86f805dbcc71cd94bf87d8b088f
mahabur-rahman <annur4395@gmail.com>
2024-09-17 05:32:29 +0600

    feat: User profile added

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,11 +1,75 @@
+import {
+  CalendarOutlined,
+  WomanOutlined,
+  PhoneOutlined,
+  AlertOutlined,
+  SafetyOutlined,
+} from "@ant-design/icons";
+import { userInfo } from "../data/data";
 
-
-const UserProfile =()=> {
+const UserProfile = () => {
   return (
-    <section>
-     dfd
-    </section>
-  )
-}
+    <>
+      <section className="max-w-sm mx-auto overflow-hidden bg-white rounded-lg">
+        <div className="relative">
+          <img
+            src={userInfo.profileImage}
+            alt="User"
+            className="w-[150px] h-[150px] mx-auto border-4 border-white rounded-full"
+          />
+        </div>
+        <div className="my-3 text-center">
+          <h2 className="text-2xl font-semibold">{userInfo.name}</h2>
+        </div>
 
-export default UserProfile
+        <div className="px-6 my-6">
+          <div className="flex items-center mb-2">
+            <CalendarOutlined className="mr-2 text-gray-600" />
+            <span>
+              Date of Birth <br />
+              <span className="font-semibold">{userInfo.dob}</span>
+            </span>
+          </div>
+          <div className="flex items-center mb-2">
+            <WomanOutlined className="mr-2 text-gray-600" />
+            <span>
+              Gender: <br />
+              <span className="font-semibold">{userInfo.gender}</span>
+            </span>
+          </div>
+          <div className="flex items-center mb-2">
+            <PhoneOutlined className="mr-2 text-gray-600" />
+            <span>
+              Contact Info <br />{" "}
+              <span className="font-semibold">{userInfo.contactInfo}</span>
+            </span>
+          </div>
+          <div className="flex items-center mb-2">
+            <AlertOutlined className="mr-2 text-gray-600" />
+            <span>
+              Emergency Contacts <br />{" "}
+              <span className="font-semibold">{userInfo.emergencyContact}</span>
+            </span>
+          </div>
+          <div className="flex items-center mb-2">
+            <SafetyOutlined className="mr-2 text-gray-600" />
+            <span>
+              Insurance Provider <br />
+              <span className="font-semibold">
+                {userInfo.insuranceProvider}
+              </span>
+            </span>
+          </div>
+        </div>
+
+        <div className="mb-6 text-center">
+          <button className="bg-[#01F0D0] text-black py-2 px-6 rounded-full font-semibold hover:bg-[#01d0b9] focus:outline-none focus:ring-2 focus:ring-[#01F0D0] focus:ring-opacity-50">
+            Show All Information
+          </button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default UserProfile;
--- src/data/data.tsx
+++ src/data/data.tsx
@@ -127,3 +127,14 @@ export const sideMenuItems = [
     rightIcon: <EllipsisOutlined />,
   },
 ];
+
+export const userInfo = {
+  name: "Jessica Taylor",
+  dob: "August 23, 1996",
+  gender: "Female",
+  contactInfo: "(415) 555-1234",
+  emergencyContact: "(415) 555-5678",
+  insuranceProvider: "Sunrise Health Assurance",
+  profileImage:
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+};
